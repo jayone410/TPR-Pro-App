fad3dd7a4357e71c0a08299f02fa88d77f70a11a
jayone410 <jayone410@googlemail.com>
2026-08-07 09:25:00 +0200

    Create rules.js

---
+++ js/js/rules.js
@@ -0,0 +1,14 @@
+const rules = {
+  topstep: {
+    payout: {
+      minTradingDays: 3,
+      recommendedBuffer: 1500
+    }
+  },
+  lucid: {
+    payout: {
+      minimumAmount: 500,
+      recommendedBuffer: 2100
+    }
+  }
+};
